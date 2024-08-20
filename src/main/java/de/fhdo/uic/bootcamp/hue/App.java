301b6e3d9bf09cb2675ebad1c58184b41bd6339c
Marcel Mitas <marcel.mitas001@stud.fh-dortmund.de>
2024-08-20 13:54:03 +0200

    Updated the UIC Hue example to an escape room element: - Modified the Hue bridge credentials so that the IP address, the user name and the light number can now be entered as java arguments. - Added possibility to ask for a code before the Hue light can be controled. - Changed the control of the Hue light from a menu-based approach to a unchangable emit of a color coded number. Each number from 0 to 9 is encoded into a light color that will be emitted 1.5 seconds long. If the emit of the color coded number is over, the light will be set off for 5 seconds and repeats the number 5 times. - Removed unused methods. - Fixed deprecation notes.

--- src/main/java/de/fhdo/uic/bootcamp/hue/App.java
+++ src/main/java/de/fhdo/uic/bootcamp/hue/App.java
@@ -3,10 +3,14 @@ package de.fhdo.uic.bootcamp.hue;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
+import java.util.*;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import com.google.common.primitives.Floats;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -15,26 +19,24 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 /**
- * @author Jonas Sorgalla
- * Example appication to demonstrate the Philips Hue System as an IoT device.
+ * @author Jonas Sorgalla (and Marcel Mitas ;))
+ * Example application to demonstrate the Philips Hue System as an IoT device.
  */
 public class App {
 
-	// Im Folgenden finden sich Variablen, die für die Verbindung zur Hue Bridge benötigt werden
-	//
-	// Die IP Adresse der Bridge, hier eine fiktive IP eingetragen. Muss geändert werden.
-	final static String HUE_ADDRESS = "http://10.0.102.26/api/";
-	// Die User ID muss initial angelegt werden. Ist der Nutzer einmal vorhanden, kann er von mehreren Personen genutzt werden.
-	// Neue Nutzer können durch das betätigen des "Buttons" auf der Bridge + ein POST Call auf die Bridge angelegt werden
-	// Eine Anleitung dazu findet sich auf https://developers.meethue.com/develop/get-started-2/
-	final static String USER = "7UvAqtykWojX4fjAnmFPJttdmi8N1erMFBMX-wCT";
-	// Jede an die Bridge angeschlossene Leuchte verfügt über eine eigene lokale ID, diese kann auch über Menüpunkt 5 ausgelesen werden
-	final static String LIGHT_NUMBER = "8";
+	// Fake IP address of the Hue Bridge. Please change it!
+	static String HUE_ADDRESS = "http://192.168.4.84/api/";
 	
-	//Variablen für die Wetter API von openweathermap.org
+	// Initial user ID required to access the Hue Bridge. Please change it!
+	// Have a look here to change it: https://developers.meethue.com/develop/get-started-2/
+	static String USER = "mL-H7OIf7wEncVofkUCsCng2jNJaJR404DSM4BUu";
+	// The number of the light to control. Change it to the number of your light.
+	static String LIGHT_NUMBER = "7";
+	
+	//Weather API for getting the current temperature
 	final static String WEATHER_API = "http://api.openweathermap.org/data/2.5/weather";
 	final static String CITY = "Dortmund";
-	final static String APIKEY = "92647b8f3a3d7920948e20505514b882";
+	final static String APIKEY = "Take your own ;)";
 	
 
 	final static OkHttpClient CLIENT = new OkHttpClient();
@@ -43,144 +45,193 @@ public class App {
 	public static void main(String[] args) throws Exception {
 		var app = new App();
 
+		// Here, the java arguments are read in to configure the access to the Hue bridge and the lights.
+		// Possible values:
+		// 1. argument (args[0]): IP address of the Hue bridge
+		// 2. argument (args[1]): Username for the Hue Bridge
+		// 3. argument (args[2]): Light number (optional)
+		if(args != null && args.length > 0) {
+			System.out.println("Lese Argumente ein...");
+			if(args.length >= 2) {
+				//First argument is the IP address. Additionally, we check the IP address format.
+				String ipaddress = args[0];
+				if(!ipaddress.matches("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}")){
+					System.out.println("Schade, das erste Argument ist keine IP-Adresse. Beende Programm.");
+					System.exit(1);
+				}
+				else {
+					HUE_ADDRESS = "http://"+ipaddress+"/api/";
+				}
+				USER = args[1];
+				if(args.length >= 3) {
+					System.out.println("Lese Nummer der Lampe ein...");
+					LIGHT_NUMBER = args[2];
+					if(!LIGHT_NUMBER.matches("[0-9]+")) {
+						System.out.println("Schade, das dritte Argument ist keine Zahl. Starte also Lampe " + LIGHT_NUMBER + ".");
+					}
+					else {
+						System.out.println("OK, starte Lampe" + LIGHT_NUMBER + ".");
+					}
+				}
+				else {
+					System.out.println("Alles startbereit. Die Lampe kenne ich nicht, also gehe ich von Lampe " + LIGHT_NUMBER + " aus.");
+				}
+			}
+		}
+		else {
+			System.out.println("Keine Argumente gefunden. Starte mit Standardwerten.");
+		}
 
-		// Erzeugen von später benötigten Objekten zur Interaktion mit der Konsole
+		// Creating objects needed for the interaction with the user
 		var scan = new Scanner(System.in);
-		//
-		System.out.println("Willkommen zur Hue TestApp im Schüler Bootcamp des User Innovation Center.");
-		System.out.println("Bitte geben Sie die entsprechende Zahl für die Aktion ein und bestätigen Sie mit Enter.");
-		System.out.println("(1) Leuchte ausschalten");
-		System.out.println("(2) Leuchte einschalten");
-		System.out.println("(3) Farbe verändern");
-		System.out.println("(4) Aus-Ein-Aus-Ein-Aus blinken");
-		System.out.println("(5) Liste aller anschlossenen Leuchten ausgeben");	
-		System.out.println("(6) Alarm anschalten");
-		System.out.println("(7) Wettermodus");
-		System.out.println("(8) Programm beenden");
+		//Asking for a specific code to start the application
+		System.out.println("Bevor ich dir zeige, was ich so kann, brauche ich den 4-stelligen Code von dir:");
 		while(!terminate) {
-			System.out.print("Neue Eingabe 1 bis 8:");
 			int input;
 			try {
 				input = scan.nextInt();		
 				switch(input) {
-				// Hier könnt ihr durch Hinzufügen von neuen "cases" einfach neue Szenarien hinzufügen.
-				// bspw. könntet ihr versuchen einen neuen case 7 hinzuzufügen, indem (1) die Leuchte angemacht,
-				// (2) die Farbe geändert wird, und anschließend (3) die Leuchte wieder ausgeschaltet wird.
-				case 1: {
-					//Licht ausschalten - GET Request mit FALSE
-					var lightState = HUE_ADDRESS+USER+"/lights/"+LIGHT_NUMBER+"/state";
-					System.out.println("Ausschalten von Leuchte "+LIGHT_NUMBER+" wird ausgeführt:\n"+lightState);
-					System.out.println("Erhaltene Antwort:\n"+app.changeState(lightState, false));
-					break;
-				}
-				case 2: {
-					//Licht einschalten - GET Request mit TRUE
-					var lightState = HUE_ADDRESS+USER+"/lights/"+LIGHT_NUMBER+"/state";
-					System.out.println("Ausschalten von Leuchte "+LIGHT_NUMBER+" wird ausgeführt:\n"+lightState);
-					System.out.println("Erhaltene Antwort:\n"+app.changeState(lightState, true));
-					break;
-				}
-				case 3: {
-					//Philips Hue erlaubt einen Farbwert von 0 bis 65353 - einfach mal ausprobieren welche Zahl welche Farbe ergibt (HSB Farbmodell)
-					var changeColor = HUE_ADDRESS+USER+"/lights/"+LIGHT_NUMBER+"/state";
-					System.out.println("Farbänderung für Leuchte "+LIGHT_NUMBER+" wird ausgeführt:\n"+changeColor);
-					System.out.println("Auf welchen Wert soll die Farbe verändert werden? Eingaben von 0 bis 65353 möglich.");
-					System.out.print("Farbwert: ");
-					var farbwert = scan.nextInt();	
-					System.out.println(app.changeColor(changeColor, farbwert));
-					break;
-				}
-				case 4: {
-					//Licht ausschalten - GET Request mit FALSE
-					var lightState = HUE_ADDRESS+USER+"/lights/"+LIGHT_NUMBER+"/state";
-					System.out.println("Zwiemaliges Blinken für Leuchte "+LIGHT_NUMBER+" wird ausgeführt:\n"+lightState);
-					System.out.println("Erhaltene Antwort:\n"+app.changeState(lightState, false));
-					// sleep() nimmt Millisekunden entgeben; 1000 Millisekunden = 1 Sekunde
-					Thread.sleep(1000);
-					System.out.println("Erhaltene Antwort:\n"+app.changeState(lightState, true));
-					Thread.sleep(1000);
-					System.out.println("Erhaltene Antwort:\n"+app.changeState(lightState, false));
-					Thread.sleep(1000);
-					System.out.println("Erhaltene Antwort:\n"+app.changeState(lightState, true));
-					Thread.sleep(1000);
-					System.out.println("Erhaltene Antwort:\n"+app.changeState(lightState, false));
-					break;
-				}
-				case 5: {
-					var allLights = HUE_ADDRESS+USER+"/lights";
-					System.out.println("Alle Lichter werden abgerufen:\n"+allLights);
-					System.out.println("\nAntwort:\n"+app.getRequest(allLights));
-					break;
-				}	
-				case 6: {
-					var alert = HUE_ADDRESS+USER+"/lights/"+LIGHT_NUMBER+"/state";
-					System.out.println("Alarm für "+LIGHT_NUMBER+" wird ausgeführt:\n"+alert);
-					System.out.println("Erhaltene Antwort:\n"+app.alertHue(alert));
-					break;
-				}
-				case 7: {
-					System.out.println("Erhaltene Antwort:\n"+app.getTemperature());
-					break;
-				}
-				case 8: {
+				case 2874: {
+					System.out.println("Das war korrekt! Jetzt geht's los...");
+					app.sendColorCode();
 					terminate = true;
 					break;
 				}
-				default:
-					System.out.println("Leider ist etwas mit der Eingabe schief gelaufen. Zulässig sind nur Zahlen von 1 bis 5."
-							+ "\nIhre Eingabe war:"+input);
+				default: {
+					System.out.println("Leider ist der eingegebene Code falsch. Bitte versuche es erneut.");
+					break;
 				}
+			}
 			} catch (InputMismatchException ime) {
 				System.out.println("Fehlerhafte Eingabe. Programm wird beendet.");
 			}
 		}
+		scan.close();
 	}
 
-	public String getRequest(String url) {
-		Request request = new Request.Builder().url(url).get().build();
-		try {
-			Response response = CLIENT.newCall(request).execute();
-			return response.body().string();
-		} catch (IOException e) {
-			System.out.println("Leider ist bei der Verbindung ein Fehler aufgetreten... :-/");
-			e.printStackTrace();
-			return null;
-		}		
-	}
+	/**
+	 * Method to send a color code to the Philips Hue light
+	 * @throws InterruptedException
+	 */
 	
+	public void sendColorCode() throws InterruptedException {
+		var lightState = HUE_ADDRESS+USER+"/lights/"+LIGHT_NUMBER+"/state"; // Die Adresse
+		for(int i = 0; i <= 5; i++) {
+			for (Integer color : getColorCode()) {
+				List<Integer> rgb = colorCodeToRGB(color);
+				int sat = rgb.size() > 3 ? rgb.get(3) : 254;
+				int bri = rgb.size() > 4 ? rgb.get(4) : 254;
+
+				changeColor(lightState, getRGBtoPhilipsHue(rgb.get(0), rgb.get(1), rgb.get(2)), sat, bri);
+				Thread.sleep(1500);
+			}
+			changeState(lightState, false);
+			Thread.sleep(5000);		
+		}
+	}
+
+	/**
+	 * Method to get the color code being sent to the Philips Hue light
+	 * @return List of numbers representing the color code
+	 */
+
+	public List<Integer> getColorCode() {
+		return Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9); //This shows all colors available in the color code. Feel free to change it.
+	}
+
+	public List<Integer> colorCodeToRGB(int colorCode) {
+		List<Integer> result = new ArrayList<>();
+		//Array pattern: [Red, Green, Blue, Saturation, Brightness] (two last values are optional)
+		switch(colorCode) {
+			case 0: { // Red
+				result = Arrays.asList(255, 0, 0);
+				break;
+			}
+			case 1: { // Orange
+				result = Arrays.asList(255, 128, 0);
+				break;
+			}
+			case 2: { // Magenta
+				result = Arrays.asList(255, 0, 255);
+				break;
+			}
+			case 3: { // Yellow
+				result = Arrays.asList(255, 255, 0);
+				break;
+			}
+			case 4: { // Green
+				result = Arrays.asList(0, 255, 0);
+				break;
+			}
+			case 5: { // Cyan / Turquoise
+				result = Arrays.asList(0, 255, 255);
+				break;
+			}
+			case 6: { // Blue
+				result = Arrays.asList(0, 0, 255);
+				break;
+			}
+			case 7: { // White
+				result = Arrays.asList(255, 255, 255, 0, 24);
+				break;
+			}
+			case 8: { // Pink
+				result = Arrays.asList(255, 204, 230, 74, 254);
+				break;
+			}
+			case 9: { // Purple
+				result = Arrays.asList(102, 0, 204);
+				break;
+			}
+			default: { // Black
+				result = Arrays.asList(255, 255, 255, 0, 24);
+				break;
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Method to change the state of the Philips Hue light
+	 * @param url The URL of the light
+	 * @param on The state of the light
+	 * @return The response of the REST call
+	 */
 
 	public String changeState(String url, boolean on) {
 		MediaType mediaType = MediaType.parse("application/json");
 
-		RequestBody body;	
-		if(on) {
-			// Schrägstriche werden für das sogenannte Escapen der Anführungszeichen benötigt!
-			body = RequestBody.create(mediaType, "{\"on\":true}\n");
-		} else {
-			body = RequestBody.create(mediaType, "{\"on\":false}\n");
-		}
+		RequestBody body;
+		String content = "{\"on\":" + (on ? "true" : "false") + "}\n";
+		body = RequestBody.create(content, mediaType);
 
 		Request request = new Request.Builder()
-				.url(url)
-				.put(body)
-				.addHeader("Content-Type", "application/json")
-				.build();
+			.url(url)
+			.put(body)
+			.addHeader("Content-Type", "application/json")
+			.build();
 
 		try {
 			Response response = CLIENT.newCall(request).execute();
 			return response.body().string();
 		} catch (IOException e) {
-			System.out.println("Leider ist bei der Verbindung ein Fehler aufgetreten... :/");
+			System.out.println("Hmm, da ist wohl was schief gelaufen und es ist meine Schuld... :/");
 			e.printStackTrace();
 			return null;
 		}		
 	}
 	
+	/**
+	 * Method to 
+	 * @param url
+	 * @return
+	 */
+
 	public String alertHue(String url) {
 		MediaType mediaType = MediaType.parse("application/json");
 		
-		// Hier wird der Body des REST Calls definiert
-		// Parameter siehe: http://www.burgestrand.se/hue-api/api/lights/
+		// Here, the body of the REST call is set.
+		// Have a look here for the parameters: http://www.burgestrand.se/hue-api/api/lights/
 		JSONObject jsonObject = new JSONObject();
 		try {
 		    jsonObject.put("alert", "lselect");
@@ -189,7 +240,7 @@ public class App {
 		    e.printStackTrace();
 		}
 
-		RequestBody body = RequestBody.create(mediaType, jsonObject.toString());
+		RequestBody body = RequestBody.create(jsonObject.toString(), mediaType);
 		
 
 		Request request = new Request.Builder()
@@ -230,10 +281,11 @@ public class App {
 		}		
 	}
 
-	public String changeColor(String url, int farbwert) {
+	public String changeColorXY(String url, float x, float y, Integer... satBri) {
 		MediaType mediaType = MediaType.parse("application/json");
-
-		RequestBody body = RequestBody.create(mediaType, "{\"on\":true, \"sat\":254, \"bri\":254,\"hue\":"+farbwert+"}\n");
+		int sat = satBri.length > 0 ? satBri[0]: 254;
+		int bri = satBri.length > 1 ? satBri[1]: 254;
+		RequestBody body = RequestBody.create("{\"on\":true, \"sat\":" + sat + ", \"bri\":" + bri + ",\"xy\":["+x+", " + y + "]}\n",mediaType);
 
 		Request request = new Request.Builder()
 				.url(url)
@@ -250,4 +302,117 @@ public class App {
 			return null;
 		}	
 	}
+
+	public String changeColor(String url, int farbwert, Integer... satBri) {
+		MediaType mediaType = MediaType.parse("application/json");
+		int sat = satBri.length > 0 ? satBri[0]: 254;
+		int bri = satBri.length > 1 ? satBri[1]: 254;
+		RequestBody body = RequestBody.create("{\"on\":true, \"sat\":" + sat + ", \"bri\":" + bri + ",\"hue\":"+farbwert+"}\n", mediaType);
+
+		Request request = new Request.Builder()
+				.url(url)
+				.put(body)
+				.addHeader("Content-Type", "application/json")
+				.build();
+
+		try {
+			Response response = CLIENT.newCall(request).execute();
+			return response.body().string();
+		} catch (IOException e) {
+			System.out.println("Leider ist bei der Verbindung ein Fehler aufgetreten... :/");
+			e.printStackTrace();
+			return null;
+		}	
+	}
+
+	/**
+	 * Method to convert any RGB color to Philips Hue Color
+	 * @param red The red value between 0 and 255
+	 * @param green The green value between 0 and 255
+	 * @param blue The blue value between 0 and 255
+	 * @return 
+	 */
+	public static int getRGBtoPhilipsHue(float red, float green, float blue) {
+		float min = red > green ? green : red;
+		min = min < blue ? min : blue;
+		float max = red > green ? red : green;
+		max = max > blue ? max : blue;
+		float delta = max - min;
+
+		float hue = 0;
+		if(delta < 0.00001) {
+			return (int) hue;
+		}
+
+		if(max == 0.0) {
+			return (int) hue;
+		}
+		if(red >= max) {
+			hue = (green - blue) / delta;
+		}
+		else if(green >= max) {
+			hue = 2.0f + (blue - red) / delta;
+		}
+		else {
+			hue = 4.0f + (red - green) / delta;
+		}
+		hue *= 60.0;
+	
+		if( hue < 0.0 ) {
+			hue += 360.0;
+		}
+		
+		float huePhilips = (hue / 360) * 65353; // Convert the Hue value (0-360°) to a Philips Hue value (0-65353)
+
+		return (int) huePhilips;
+}
+
+public static List<Float> getRGBtoXY(float red, float green, float blue) {
+    // For the hue bulb the corners of the triangle are:
+    // -Red: 0.675, 0.322
+    // -Green: 0.4091, 0.518
+    // -Blue: 0.167, 0.04
+    double[] normalizedToOne = new double[3];
+    normalizedToOne[0] = (red / 255);
+    normalizedToOne[1] = (green / 255);
+    normalizedToOne[2] = (blue / 255);
+    float vividRed, vividGreen, vividBlue;
+
+    // Make red more vivid
+    if (normalizedToOne[0] > 0.04045) {
+        vividRed = (float) Math.pow(
+                (normalizedToOne[0] + 0.055) / (1.0 + 0.055), 2.4);
+    } else {
+        vividRed = (float) (normalizedToOne[0] / 12.92);
+    }
+
+    // Make green more vivid
+    if (normalizedToOne[1] > 0.04045) {
+        vividGreen = (float) Math.pow((normalizedToOne[1] + 0.055)
+                / (1.0 + 0.055), 2.4);
+    } else {
+        vividGreen = (float) (normalizedToOne[1] / 12.92);
+    }
+
+    // Make blue more vivid
+    if (normalizedToOne[2] > 0.04045) {
+    	vividBlue = (float) Math.pow((normalizedToOne[2] + 0.055)
+                / (1.0 + 0.055), 2.4);
+    } else {
+        vividBlue = (float) (normalizedToOne[2] / 12.92);
+    }
+
+    float X = (float) (vividRed * 0.649926 + vividGreen * 0.103455 + vividBlue * 0.197109);
+    float Y = (float) (vividRed * 0.234327 + vividGreen * 0.743075 + vividBlue * 0.022598);
+    float Z = (float) (vividRed * 0.0000000 + vividGreen * 0.053077 + vividBlue * 1.035763);
+
+    float x = X / (X + Y + Z);
+    float y = Y / (X + Y + Z);
+
+    float[] xy = new float[2];
+    xy[0] = x;
+    xy[1] = y;
+    List<Float> xyAsList = Floats.asList(xy);
+    return xyAsList;
+}
 }
